328f8c9eae0eafe6f610e50108830181f81d1b37
Ando <feodan@yandex.ru>
2017-10-06 09:06:38 +0300

    Initial commit

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,91 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.By.xpath;
+
+public class HomePage {
+    private final WebDriver driver;
+    private String todoCheckboxLocator = "//*[text()=''{0}'']/../input";
+    private String todoLabelText = "//*[text()=''{0}'']";
+//    private By todoInputLocator = cssSelector("[ng-model='todoList.todoText']");
+//    private By addTodoButtonLocator = cssSelector("[type='submit']");
+    WebElement todoInput = driver.findElement(cssSelector("[ng-model='todoList.todoText']"));
+    WebElement addTodoButton = driver.findElement(cssSelector("[type='submit']"));
+    private By todoItemsLocator = cssSelector("ul.unstyled li span");
+    private By archiveLocator = cssSelector("[ng-click='todoList.archive()']");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+
+        if (!"AngularJS — Superheroic JavaScript MVW Framework".equals(driver.getTitle())) {
+            throw new IllegalStateException("This is not the home page");
+        }
+    }
+
+    /**
+     * Finds to_do with specific name and checks it
+     *
+     * @param todoName specify to_do name
+     * @return home page
+     */
+    public HomePage checkTodo(String todoName) {
+        driver.findElement(xpath(MessageFormat.format(todoCheckboxLocator, todoName))).click();
+        return this;
+    }
+
+    /**
+     * Adds new to_do by typing to_name and pressing Add button
+     *
+     * @param todoName specify to_do name
+     * @return home page
+     */
+    public HomePage addTodo(String todoName) {
+//        driver.findElement(todoInputLocator).sendKeys(todoName);
+//        driver.findElement(addTodoButtonLocator).click();
+        todoInput.sendKeys(todoName);
+        addTodoButton.click();
+        return this;
+    }
+
+    /**
+     * Verifies specified to_do in the list
+     *
+     * @param todoName specify to_do name
+     * @return true id to_do is presented in the list and false if not presented
+     */
+    public boolean checkTodoPresence(String todoName) {
+        List<WebElement> myElements = driver.findElements(todoItemsLocator);
+        for (WebElement e : myElements) {
+            if (e.getText().equals(todoName))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Archives specified to_do by checking it and pressing Archive button
+     *
+     * @param todoName specify to_do name
+     * @return home page
+     */
+    public HomePage archiveTodo(String todoName) {
+        checkTodo(todoName);
+        driver.findElement(archiveLocator).click();
+        return this;
+    }
+
+    /**
+     * Verifies selection of specified to_do
+     *
+     * @param todoName specify to_do name
+     * @return true if specified to_do is checked and false if to_do is unchecked
+     */
+    public boolean checkSelectionOfTodo(String todoName) {
+        return driver.findElement(xpath(MessageFormat.format(todoLabelText, todoName))).getAttribute("class").equals("done-true");
+    }
+}
---
+++ src/test/java/TodoFunctionalityTest.java
@@ -0,0 +1,64 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.text.MessageFormat;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TodoFunctionalityTest {
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:/dev/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.get("https://angularjs.org/");
+    }
+
+    /**
+     * Test verifies that to_do is displayed on UI after its creation and disappearing after its removing
+     */
+    @Test
+    public void testAddRemoveTodo() {
+        HomePage homePage = new HomePage(webDriver);
+        String todoName = "AAA";
+
+        homePage.addTodo(todoName);
+        assertTrue(MessageFormat.format("Can''t find {0} todo", todoName), homePage.checkTodoPresence(todoName));
+
+        homePage.archiveTodo(todoName);
+        assertFalse(MessageFormat.format("Can find {0} todo", todoName), homePage.checkTodoPresence(todoName));
+    }
+
+    /**
+     * Test verifies that to_do after checking becomes strikethrough and still visible.
+     * After unchecking to_do becomes unstrikethrough and still visible.
+     */
+    @Test
+    public void testCheckUncheckTodo() {
+        HomePage homePage = new HomePage(webDriver);
+        String todoName = "AAA";
+
+        homePage.addTodo(todoName);
+        homePage.checkTodo(todoName);
+        assertTrue(MessageFormat.format("Can''t find {0} todo", todoName), homePage.checkTodoPresence(todoName));
+        assertTrue(MessageFormat.format("{0} todo isn''t selected", todoName),homePage.checkSelectionOfTodo(todoName));
+
+        homePage.checkTodo(todoName);
+        assertTrue(MessageFormat.format("Can''t find {0} todo", todoName), homePage.checkTodoPresence(todoName));
+        assertFalse(MessageFormat.format("{0} todo is selected", todoName),homePage.checkSelectionOfTodo(todoName));
+
+        homePage.archiveTodo(todoName);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+}
